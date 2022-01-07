6ac7dcf721cffc1bdaff1ef586e6675a0c2efe9e
Walter Diaz <walter@osigu.com>
2022-01-06 20:12:33 -0600

    Segunda Prueba

--- cypress/integration/form.spec.js
+++ cypress/integration/form.spec.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 import '@percy/cypress';
-describe('Ejemplo de uso de percy', () => {
+describe('Ejemplo de uso de per', () => {
     it('Desplegando el formulario del paciente', () => {
         cy.visit("http://192.168.1.21:3030/")
         cy.get('[data-cy=new-patient]').click()
